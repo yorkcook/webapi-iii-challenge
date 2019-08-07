f639acc2bc2e5c3871626b7fc140907604116eeb
York Cook <yorkcook@Johnny-Castle.lan1>
2019-08-07 16:13:18 -0400

    WIP: most CRUD for users built out

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+
+const express = require("express");
+
+const server = express();
+
+const userRouter = require("./users/userRouter");
+
+const helmet = require("helmet");
+
+server.use(express.json());
+
+server.use("/", userRouter);
+
+server.listen(4000, () => {
+  console.log("Server reporting for duty from port 4000!");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,73 @@
-const express = 'express';
+const express = "express";
 
-const router = express.Router();
+const router = require("express").Router();
 
-router.post('/', (req, res) => {
+let User = require("./userDb.js");
 
+router.post("/", (req, res) => {});
+
+router.post("/:id/posts", (req, res) => {});
+
+router.get("/", (req, res) => {
+  User.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "You have been denied the user list!" });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    User.getById(id)
+      .then(user => {
+        res.status(200).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({ message: "User retrieval failed!" });
+      });
+  } else {
+    res.status(404).json({ message: "No user exists with that id!" });
+  }
 });
 
-router.get('/', (req, res) => {
-
+router.get("/:id/posts", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    User.getUserPosts(id)
+      .then(posts => {
+        res.status(200).json(posts);
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Oh no! Can't retrieve the posts!" });
+      });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
 
+  //if (id) {
+  User.remove(id)
+    .then(user => {
+      res.status(200).json({ message: "User has been eliminated." });
+    })
+    .catch(user => {
+      res.status(500).json({ error: "You can not perform that elimination!" });
+    });
+  //}
 });
 
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
